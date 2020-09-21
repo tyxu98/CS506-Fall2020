96e4c8b8da55f47c7b36d9c614a4de74c9969e90
Zhan Hao (Tiger) Xu <tyxu98@bu.edu>
2020-09-21 18:18:55 -0400

    read_cvs complete

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,16 @@
+import csv
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    check = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    final = []
+    f = open(csv_file_path)
+    readFile = csv.reader(f)
+    for row in readFile:
+        for i in range(len(row)):
+            if row[i] not in check:
+                row[i] = int(row[i])
+        final+=[row]
+    return final
